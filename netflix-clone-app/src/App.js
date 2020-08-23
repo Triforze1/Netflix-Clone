627c9d328767f8e7cc05526e5ca72556ad79892d
Triforze1 <b_farrill@ymail.com>
2020-08-23 01:04:19 -0400

    Set Base URL/Api key, started building out row

--- netflix-clone-app/src/App.js
+++ netflix-clone-app/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './css/App.css';
+import Row from './components/Row';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Everyone</h1>
+      <Row title="NETFLIX ORIGINALS"/>
+      <Row title="Trending Now"/>
+      <Row title="Top Rated"/>
+      <Row title="Action"/>
+      <Row title="Comedy"/>
+      <Row title="Horror"/>
+      <Row title="Romance"/>
+      <Row title="Documentaries"/>
     </div>
   );
 }
---
+++ netflix-clone-app/src/components/Row.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function Row({ title }) {
+
+    const [movies, setMovies] = useState([]);
+
+    
+
+    return (
+        <div>
+            <h2>{title}</h2>
+            {/* Container -> posters */}
+            
+        </div>
+    )
+}
+
+export default Row
